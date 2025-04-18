f7e81e8329c15396eff14a2a2bed87aa1cbdb7e5
Caio Andrade <caio.andrade2442@gmail.com>
2025-04-18 17:17:20 -0300

    Primeiro exercicio

---
+++ CadastroProdutos/Program.cs
@@ -0,0 +1,86 @@
+﻿using System.Globalization;
+
+namespace CadastroProdutos
+{
+
+    public class Produto
+    {
+        public string Nome;
+        public double Preco;
+        public int Quantidade;
+
+        public double ValorTotalEmEstoque()
+        {
+            return Preco * Quantidade;
+        }
+
+        public void AdicionarProdutos(int quantidade)
+        {
+            Quantidade += quantidade;
+        }
+
+        public void RemoverProdutos(int quantidade)
+        {
+            Quantidade -= quantidade;
+        }
+
+        public override string ToString()
+        {
+            return Nome
+                + ", $ "
+                + Preco.ToString("F2", CultureInfo.InvariantCulture)
+                + ", "
+                + Quantidade
+                + " unidades, Total: $ "
+                + ValorTotalEmEstoque().ToString("F2", CultureInfo.InvariantCulture);
+        }
+    }
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Produto produto = new Produto();
+
+            Console.WriteLine("Entre os dados do produto:");
+            Console.Write("Nome: ");
+            produto.Nome = Console.ReadLine();
+            Console.Write("Preço: ");
+            produto.Preco = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            Console.Write("Quantidade no estoque: ");
+            produto.Quantidade = int.Parse(Console.ReadLine());
+
+            Console.WriteLine();
+            Console.WriteLine("Dados do produto: " + produto);
+
+            Console.WriteLine();
+            Console.Write("Você deseja adicionar mais produtos ao estoque? (S/N): ");
+            char pergunta = char.Parse(Console.ReadLine());
+
+            if (pergunta == 'N' || pergunta == 'n')
+            {
+                System.Environment.Exit(1);
+            }
+
+            Console.Write("Digite o número de produtos a ser adicionado ao estoque: ");
+            int qte = int.Parse(Console.ReadLine());
+            produto.AdicionarProdutos(qte);
+
+            Console.WriteLine("Dados atualizados: " + produto);
+
+            Console.WriteLine();
+            Console.Write("Você deseja remover produtos do estoque? (S/N): ");
+            pergunta = char.Parse(Console.ReadLine());
+
+            if (pergunta == 'N' || pergunta == 'n')
+            {
+                System.Environment.Exit(1);
+            }
+
+            Console.Write("Digite o número de produtos a ser removido do estoque: ");
+            qte = int.Parse(Console.ReadLine());
+            produto.RemoverProdutos(qte);
+
+            Console.WriteLine("Dados atualizados: " + produto);
+        }
+    }
+}
