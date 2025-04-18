5c4d0e81ffba9b5fd85aef013a9cdfd8b62242ac
Caio Andrade <caio.andrade2442@gmail.com>
2025-04-18 19:12:58 -0300

    Exemplo e exercicio membros estaticos

---
+++ ConversorMoedas/Program.cs
@@ -0,0 +1,30 @@
+﻿using System.Globalization;
+
+namespace ConversorMoedas
+{
+    public class ConversorDeMoeda 
+    {
+        public static double IOF = 6;
+        
+        public static double Calculo(double cotacao, double dolaresAComprar)
+        {
+           double valorIof = (IOF / 100) * (cotacao * dolaresAComprar);
+           return dolaresAComprar * cotacao + valorIof;
+        }
+
+    }
+    
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Qual é a cotação do dólar? ");
+            double cotacao = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            Console.Write("Quantos dólares você vai comprar? ");
+            double dolaresAComprar = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            Console.Write("Valor a ser pago em reais = " + ConversorDeMoeda.Calculo(cotacao, dolaresAComprar)
+                                                                .ToString("F2"), CultureInfo.InvariantCulture);
+        }
+    }
+}
---
+++ MembrosEstaticosExemplo/Program.cs
@@ -0,0 +1,52 @@
+﻿using System.Globalization;
+
+namespace MembrosEstaticosExemplo
+{
+
+    public class Calculadora
+    {
+        public static double PI = 3.14;
+
+        public static double Circunferencia(double raio)
+        {
+            return 2.0 * PI * raio;
+        }
+
+        public static double Volume(double raio)
+        {
+            return 4.0 / 3.0 * PI * Math.Pow(raio, 3);
+        }
+
+    }
+
+    public class Program
+    {
+
+        //static double Pi = 3.14;
+
+        static void Main(string[] args)
+        {
+            Console.Write("Entre o valor do raio: ");
+            double raio = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            double circ = Calculadora.Circunferencia(raio);
+            Console.WriteLine("Circunferencia: " + circ.ToString("F2", CultureInfo.InvariantCulture));
+
+            double volume = Calculadora.Volume(raio);
+            Console.WriteLine("Volume: " + volume.ToString("F2", CultureInfo.InvariantCulture));
+
+            Console.WriteLine("Valor de PI: " + Calculadora.PI.ToString("F2", CultureInfo.InvariantCulture));
+        }
+
+       /* static double Circunferencia(double raio)
+        {
+            return 2.0 * PI * raio;
+        }
+
+        static double Volume(double raio)
+        {
+            return 4.0 / 3.0 * PI * Math.Pow(raio, 3);
+        }
+       */
+    }
+}
