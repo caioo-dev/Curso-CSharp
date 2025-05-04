8ed9436acf073c3a13e29b33bcc1ab993f8b1725
Caio Andrade <caio.andrade2442@gmail.com>
2025-05-04 18:22:52 -0300

    Struct

---
+++ ConsoleApp1/Point.cs
@@ -0,0 +1,14 @@
+﻿namespace ConsoleApp1
+{
+    struct Point
+    {
+
+        public double x, y;
+
+        public override string ToString()
+        {
+            return "(" + x + "," + y + ")";
+        }
+
+    }
+}
---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,16 @@
+﻿namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Point p;
+            Console.WriteLine(p); // erro: variável não atribuída
+            p.x = 10;
+            p.y = 20;
+            Console.WriteLine(p);
+            p = new Point();
+            Console.WriteLine(p);
+        }
+    }
+}
