45d35896993c85dcbd1fb6680bb90ab6adb4ff3a
Caio Andrade <caio.andrade2442@gmail.com>
2025-05-07 21:56:01 -0300

    vetores

---
+++ AverageHeight/Program.cs
@@ -0,0 +1,25 @@
+﻿using System.Globalization;
+
+namespace AverageHeight
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int N = int.Parse(Console.ReadLine());
+
+            double[] heights = new double[N];
+            double total = 0;
+            for (int i = 0; i < N; i++)
+            {
+                heights[i] = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+                total += heights[i];
+            }
+
+            double average = total / N;
+
+            Console.Write($"AVERAGE HEIGHT = {average.ToString("F2", CultureInfo.InvariantCulture)}");
+
+        }
+    }
+}
