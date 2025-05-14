86b1907d14c7f1104ed692c5b866a7da58f7e8fe
Caio Andrade <caio.andrade2442@gmail.com>
2025-05-14 20:20:50 -0300

    exercicio matriz

--- Matrizes/Program.cs
+++ Matrizes/Program.cs
@@ -4,7 +4,7 @@
     {
         static void Main(string[] args)
         {
-            double[,] mat = new double[2, 3];
+            /*double[,] mat = new double[2, 3];
 
             Console.WriteLine(mat.Length);
 
@@ -12,6 +12,44 @@
 
             Console.WriteLine(mat.GetLength(0));
             Console.WriteLine(mat.GetLength(1));
+            */
+
+            int N = int.Parse(Console.ReadLine());
+
+            int[,] arr = new int[N, N];
+
+            for (int i = 0; i < N; i++)
+            {
+
+                string[] values = Console.ReadLine().Split(' ');
+
+                for (int j = 0; j < N; j++)
+                {
+                    arr[i, j] = int.Parse(values[j]);
+                }
+            }
+
+            Console.WriteLine("Main diagonal: ");
+            for (int i = 0; i < N; i++)
+            {
+                Console.Write(arr[i, i] + "");
+            }
+            Console.WriteLine();
+
+            int count = 0;
+            for (int i = 0; i < N; i++)
+            {
+
+                for (int j = 0; j < N; j++)
+                {
+                    if (arr[i, j] < 0)
+                    {
+                        count++;
+                    }
+                }
+            }
+            Console.WriteLine($"Negative numbers: {count}");
+
         }
     }
 }
