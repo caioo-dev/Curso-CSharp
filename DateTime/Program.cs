d2459c1fe8e15ba7e30dbff7b06e25f20b8fb174
Caio Andrade <caio.andrade2442@gmail.com>
2025-05-29 22:28:04 -0300

    propriedades e datetimekind

--- DateTime/Program.cs
+++ DateTime/Program.cs
@@ -1,4 +1,5 @@
-﻿using System.Globalization;
+﻿using System;
+using System.Globalization;
 
 namespace DateTimeTestes
 {
@@ -6,7 +7,7 @@ namespace DateTimeTestes
     {
         static void Main(string[] args)
         {
-            DateTime d1 = DateTime.Now;
+            /*DateTime d1 = DateTime.Now;
 
             Console.WriteLine(d1);
             Console.WriteLine(d1.Ticks);
@@ -41,6 +42,110 @@ namespace DateTimeTestes
 
             DateTime d12 = DateTime.ParseExact("2000-08-15 13:08:58", "yyyy-MM-dd HH:mm:ss", CultureInfo.InvariantCulture);
             Console.WriteLine(d12);
+            */
+
+            /*Propriedades
+            • Date(DateTime)
+            • Day(int)
+            • DayOfWeek(DayOfWeek)
+            • DayOfYear(int)
+            • Hour(int)
+            • Kind(DateTimeKind)
+            • Millisecond(int)
+            • Minute(int)
+            • Month(int)
+            • Second(int)
+            • Ticks(long)
+            • TimeOfDay(TimeSpan)
+            • Year(int)
+            */
+
+            /*
+            DateTime d = new DateTime(2001, 8, 15, 13, 45, 58, 275);
+
+            Console.WriteLine(d);
+            Console.WriteLine("1) Date: " + d.Date);
+            Console.WriteLine("2) Day: " + d.Day);
+            Console.WriteLine("3) DayOfWeek: " + d.DayOfWeek);
+            Console.WriteLine("4) DayOfYear: " + d.DayOfYear);
+            Console.WriteLine("5) Hour: " + d.Hour);
+            Console.WriteLine("6) Kind: " + d.Kind);
+            Console.WriteLine("7) Millisecond: " + d.Millisecond);
+            Console.WriteLine("8) Minute: " + d.Minute);
+            Console.WriteLine("9) Month: " + d.Month);
+            Console.WriteLine("10) Second: " + d.Second);
+            Console.WriteLine("11) Ticks: " + d.Ticks);
+            Console.WriteLine("12) TimeOfDay: " + d.TimeOfDay);
+            Console.WriteLine("13) Year: " + d.Year);
+
+           //Formatação(DateTime-> string)
+
+            string s1 = d.ToLongDateString();
+            string s2 = d.ToLongTimeString();
+            string s3 = d.ToShortDateString();
+            string s4 = d.ToShortTimeString();
+            string s5 = d.ToString();
+            string s6 = d.ToString("yyyy-MM-dd HH:mm:ss");
+            string s7 = d.ToString("yyyy-MM-dd HH:mm:ss.fff");
+            Console.WriteLine(s1);
+            Console.WriteLine(s2);
+            Console.WriteLine(s3);
+            Console.WriteLine(s4);
+            Console.WriteLine(s5);
+            Console.WriteLine(s6);
+            Console.WriteLine(s7);
+
+            //Operações com Datetime
+            DateTime x =  
+
+            DateTime y = x.Add(timeSpan);
+            DateTime y = x.AddDays(double);
+            DateTime y = x.AddHours(double);
+            DateTime y = x.AddMilliseconds(double);
+            DateTime y = x.AddMinutes(double);
+            DateTime y = x.AddMonths(int);
+            DateTime y = x.AddSeconds(double);
+            DateTime y = x.AddTicks(long);
+            DateTime y = x.AddYears(int);
+            DateTime y = x.Subtract(timeSpan);
+            TimeSpan t = x.Subtract(dateTime);
+            */
+
+            //Demo
+            DateTime d1 = new DateTime(2000, 8, 15, 13, 5, 58, DateTimeKind.Local);
+            DateTime d2 = new DateTime(2000, 8, 15, 13, 5, 58, DateTimeKind.Utc);
+            DateTime d3 = new DateTime(2000, 8, 15, 13, 5, 58);
+            Console.WriteLine("d1: " + d1);
+            Console.WriteLine("d1 Kind: " + d1.Kind);
+            Console.WriteLine("d1 to Local: " + d1.ToLocalTime());
+            Console.WriteLine("d1 to Utc: " + d1.ToUniversalTime());
+            Console.WriteLine();
+            Console.WriteLine("d2: " + d2);
+            Console.WriteLine("d2 Kind: " + d2.Kind);
+            Console.WriteLine("d2 to Local: " + d2.ToLocalTime());
+            Console.WriteLine("d2 to Utc: " + d2.ToUniversalTime());
+            Console.WriteLine();
+            Console.WriteLine("d3: " + d3);
+            Console.WriteLine("d3 Kind: " + d3.Kind);
+            Console.WriteLine("d3 to Local: " + d3.ToLocalTime());
+            Console.WriteLine("d3 to Utc: " + d3.ToUniversalTime());
+
+            //Demo
+            DateTime d4 = DateTime.Parse("2000-08-15 13:05:58");
+            DateTime d5 = DateTime.Parse("2000-08-15T13:05:58Z"); // cria local DateTime
+            Console.WriteLine("d1: " + d4);
+            Console.WriteLine("d1 Kind: " + d4.Kind);
+            Console.WriteLine("d1 to Local: " + d4.ToLocalTime());
+            Console.WriteLine("d1 to Utc: " + d4.ToUniversalTime());
+            Console.WriteLine();
+            Console.WriteLine("d2: " + d5);
+            Console.WriteLine("d2 Kind: " + d5.Kind);
+            Console.WriteLine("d2 to Local: " + d5.ToLocalTime());
+            Console.WriteLine("d2 to Utc: " + d5.ToUniversalTime());
+            Console.WriteLine();
+            Console.WriteLine(d5.ToString("yyyy-MM-ddTHH:mm:ssZ")); // cuidado!
+            Console.WriteLine(d5.ToUniversalTime().ToString("yyyy-MM-ddTHH:mm:ssZ"));
+
         }
     }
 }
