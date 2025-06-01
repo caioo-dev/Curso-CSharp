6d5ba624ddc171cd65b5ec3d20b9bb59ccc592b3
Caio Andrade <caio.andrade2442@gmail.com>
2025-06-01 20:05:22 -0300

    enums, entities e listas

---
+++ WorkerSalary/Entities/Department.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WorkerSalary.Entities
+{
+    internal class Department
+    {
+        public string Name { get; private set; }
+
+        public Department()
+        {
+            
+        }
+
+        public Department(string name)
+        {
+            Name = name;
+        }
+
+    }
+}
