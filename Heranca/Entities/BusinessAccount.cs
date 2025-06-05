678606c507c9d1b4c7ba45a665fcf9a85d239e41
Caio Andrade <caio.andrade2442@gmail.com>
2025-06-05 20:55:50 -0300

    herança e modificador de acesso protected

---
+++ Heranca/Entities/BusinessAccount.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Herança.Entities
+{
+    namespace Course.Entities
+    {
+        class BusinessAccount : Account
+        {
+            public double LoanLimit { get; set; }
+
+            public BusinessAccount()
+            {
+            }
+
+            public BusinessAccount(int number, string holder, double balance, double loanLimit)
+                : base(number, holder, balance)
+            {
+                LoanLimit = loanLimit;
+            }
+
+            public void Loan(double amount)
+            {
+                if (amount <= LoanLimit)
+                {
+                    Balance += amount;
+                }
+            }
+        }
+    }
+}
---
+++ Heranca/Program.cs
@@ -0,0 +1,17 @@
+﻿using Herança.Entities.Course.Entities;
+
+namespace Herança
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            BusinessAccount account = new BusinessAccount(8010, "Bob Brown", 100.0, 500.0);
+
+            Console.WriteLine(account.Balance);
+
+            //account.Balance = 200.0; // error
+        }
+    }
+
+}
